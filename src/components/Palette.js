d76cf1abe9c439cf4dda5b1facc7f4c3a611c980
Julian Hoyos <jhoyos135@gmail.com>
2019-06-02 19:32:55 -0400

    added slider to change color values

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import './Palette.css'
 
 
 export class Palette extends Component {
-    render() {
 
-        const colorBoxes = this.props.colors.map(color => {
-            return <ColorBox background={color.color} name={color.name} />
+    state = {
+        level: 500
+    };
+
+    changeLevel = (level) => {
+        this.setState({
+            level
+        })
+    };
+    
+    render() {
+        const {level} = this.state;
+        const colorBoxes = this.props.palette.colors[level].map(color => {
+            return <ColorBox background={color.hex} name={color.name} />
         });
 
         return (
             <div className='Palette'>
+                <div className='slider'>
+                    <Slider
+                    defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changeLevel}
+                    />
+                 </div>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div> 
