a012a37189deaaf0c745e22501f1a9fdef507db1
Julian Hoyos <jhoyos135@gmail.com>
2019-06-27 18:36:42 -0400

    work more on responsiveness and added transitions for main palette deletion

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -3,13 +3,16 @@ import MiniPalette from './MiniPalette';
 import styles from './styles/PaletteListStyles';
 import { withStyles } from '@material-ui/styles';
 import { Link } from 'react-router-dom';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 export class PaletteList extends Component {
-	renderPalettes = () => {
-		return this.props.palettes.map((palette) => {
-			return <MiniPalette {...palette} key={palette.id} handleClick={() => this.goToPalette(palette.id)} handleDelete={this.props.deletePalette} id={palette.id} />;
-		});
-	};
+	// renderPalettes = () => {
+			
+	// 			this.props.palettes.map((palette) => {
+	// 				<MiniPalette {...palette} key={palette.id} handleClick={() => this.goToPalette(palette.id)} handleDelete={this.props.deletePalette} id={palette.id} />;
+	// 		})
+			
+	// };
 
 	goToPalette = (id) => {
 		this.props.history.push(`/palette/${id}`);
@@ -23,7 +26,30 @@ export class PaletteList extends Component {
 						<h1>React-Color-App</h1>
 						<Link to="/palette/new">Create New Palette</Link>
 					</nav>
-					<div className={this.props.classes.palettes}>{this.renderPalettes()}</div>
+						
+						<TransitionGroup className={this.props.classes.palettes}> 
+							{
+								this.props.palettes.map((palette) => {
+									return (
+										<CSSTransition 
+										key={palette.id} 
+										classNames='fade' 
+										timeout={300}
+										>
+										<MiniPalette 
+										{...palette} 
+										key={palette.id} 
+										handleClick={() => this.goToPalette(palette.id)} 
+										handleDelete={this.props.deletePalette} 
+										id={palette.id} />
+									</CSSTransition>
+									)
+
+									})
+							}
+						</TransitionGroup>
+
+						
 				</div>
 			</div>
 		);
--- src/components/styles/PaletteListStyles.js
+++ src/components/styles/PaletteListStyles.js
@@ -1,17 +1,35 @@
+import sizes from './sizes';
+import bg from './bg.svg';
 export default {
+    "@global": {
+        ".fade-exit": {
+            opacity: 1
+        },
+        ".fade-exit-active": {
+            opacity: 0,
+            transition: 'opacity 0.5s ease-out'
+        }
+    },
     root: {
         backgroundColor: '#333',
+        backgroundImage: `url(${bg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
         height: '100vh',
         display: 'flex',
         alignItems: 'flex-start',
-        justifyContent: 'center'
-    },
+        justifyContent: 'center',
+        overflowY: 'scroll'
+        },
     container: {
         width: '50%',
         display: 'flex',
         alignItems: 'flex-start',
         flexDirection: 'column',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        [sizes.down('lg')]: {
+            width: '80%'
+        }
     },
     nav: {
         display: 'flex',
@@ -32,6 +50,12 @@ export default {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
+        gridGap: '1.5rem',
+        [sizes.down('md')]: {
+            gridTemplateColumns: 'repeat(2, 50%)'
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: 'repeat(1, 100%)'
+        }
     }
 };
