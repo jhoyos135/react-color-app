f25c4952b5aa03fbc477ec8c846f6e0cae0de194
Julian Hoyos <jhoyos135@gmail.com>
2019-06-27 19:40:18 -0400

    refactor delete functionality with modal

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -4,8 +4,46 @@ import styles from './styles/PaletteListStyles';
 import { withStyles } from '@material-ui/styles';
 import { Link } from 'react-router-dom';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from '@material-ui/core/Avatar'
+import blue from '@material-ui/core/colors/blue'
+import red from '@material-ui/core/colors/red'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemText from '@material-ui/core/ListItemText'
+import CheckIcon from '@material-ui/icons/Check'
+import CloseIcon from '@material-ui/icons/Close'
 
 export class PaletteList extends Component {
+
+	state = {
+		openDeleteDialog: false,
+		deletingId: ''
+	}
+
+	openDialog = (id) => {
+		this.setState({
+			openDeleteDialog: true,
+			deletingId: id
+		})
+	};
+
+	closeDialog = () => {
+		this.setState({
+			openDeleteDialog: false,
+			deletingId: ''
+		})
+	};
+
+	handleDelete = () => {
+		this.props.deletePalette(this.state.deletingId);
+		this.setState({
+			openDeleteDialog: false
+		})
+	};
+
 	// renderPalettes = () => {
 			
 	// 			this.props.palettes.map((palette) => {
@@ -40,17 +78,45 @@ export class PaletteList extends Component {
 										{...palette} 
 										key={palette.id} 
 										handleClick={() => this.goToPalette(palette.id)} 
-										handleDelete={this.props.deletePalette} 
+										//handleDelete={this.props.deletePalette} 
+										openDialog={this.openDialog}
 										id={palette.id} />
 									</CSSTransition>
 									)
 
 									})
 							}
-						</TransitionGroup>
-
-						
+						</TransitionGroup>	
 				</div>
+				<Dialog
+				open={this.state.openDeleteDialog}
+				aria-label='delete-dialog-title'
+				onClose={this.closeDialog}
+				>
+							<DialogTitle id="delete-dialog-title">Delete this Palette</DialogTitle>
+							<List>
+								<ListItem
+								onClick={this.handleDelete}
+								 button>
+									<ListItemAvatar>
+										<Avatar style={{background: blue[100], color: blue[600]}}>
+											<CheckIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText primary='Delete' />
+								</ListItem>
+								<ListItem 
+								onClick={this.closeDialog}
+								button>
+									<ListItemAvatar>
+										<Avatar style={{background: red[100], color: red[600]}}>
+											<CloseIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText primary='Cancel' />
+								</ListItem>
+							</List>
+				</Dialog>
 			</div>
 		);
 	}
